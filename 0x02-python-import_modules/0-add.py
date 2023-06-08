605be73a7959f386d156a0c6398306fe5ccfb1a2
glackycode <glackycode@gmail.com>
2023-06-08 11:50:07 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as a0
 a = 1
 b = 2
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, a0.add(a, b)))

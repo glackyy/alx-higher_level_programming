1d3d8684cfddfc15617a079322fbeb676df20034
glackycode <glackycode@gmail.com>
2023-06-08 14:59:47 +0000

    fast alphabet

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-if __name__== "__main__":
+if (__name__== "__main__"):
     import sys
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) < 4:
+    if (len(sys.argv)) < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
-    if op == '+':
+    if (op == '+'):
         res = add(a, b)
-    elif op == '-':
+    elif (op == '-'):
         res = sub(a, b)
-    elif op == '*':
+    elif (op == '*'):
         res = mul(a, b)
-    elif op == '/':
+    elif (op == '/'):
         res = div(a, b)
     else:
         print("Unknown operator, Available operators: +, -, * and /")

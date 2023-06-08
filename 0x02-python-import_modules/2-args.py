062f97aa9acb094185ad4d6ec8b3b68e8e7d930c
glackycode <glackycode@gmail.com>
2023-06-08 12:52:04 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-     args = sys.argv[1:]  # Exclude the script name itself
+     args = sys.argv[1:]
      num_args = len(args)
      if num_args == 0:
          print("0 arguments.")

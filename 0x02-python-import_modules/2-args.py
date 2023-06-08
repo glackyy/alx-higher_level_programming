3abf9290363de081449113ca4354c2f8976dcc49
glackyy <othmanesticky@gmail.com>
2023-06-08 12:22:07 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
     if len(sys.argv) > 0:
         print("{} arguments:".format(len(sys.argv)))
         for i in range(1, len(sys.argv) + 1):

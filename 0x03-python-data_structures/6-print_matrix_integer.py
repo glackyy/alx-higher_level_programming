65986e442391264bd1afe814fbadfcb4ccc83eaa
glackycode <glackycode@gmail.com>
2023-06-12 09:05:32 +0000

    print matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for r in matrix:
+        for i in range(len(r)):
+            if i < len(r) - 1:
+               print("{:d}".format(r[i]), end="")
+            else:
+                print("{:d}".format(r[i]))
+    return

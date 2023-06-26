888fadb97eba8f83633c72a8957693dbc8f5f16c
glackycode <glackycode@gmail.com>
2023-06-26 10:24:58 +0000

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+def is_valid_num_type(val):
+    return isinstance(val, (int, float))
+
 def list_division(my_list_1, my_list_2, list_length):
     n_list = []
     for i in range(list_length):
         try:
             e1 = my_list_1[i]
             e2 = my_list_2[i]
-            if isinstance(e1, (int, float)) and isinstance(e2, (int, float)):
+            if is_valid_num_type(e1) and is_valid_num_type(e2):
                 if e2 == 0:
                     raise ZeroDivisionError
                 res = e1 / e2

5635ed02b1384e46eda3f850460f6e71b591bef5
glackycode <glackycode@gmail.com>
2023-06-27 10:48:06 +0000

    Size square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,13 +3,13 @@
 
 
 class Square:
-   """class represents Square"""
+    """class represents Square"""
 
-   def __init__(self, size):
-     """Init instance of the class
-        
-     Args:
-       Size (int): The size of the square
-     """
+    def __init__(self, size):
+        """Init instance of the class
 
-     self.__size = size
+        Args:
+        Size (int): The size of the square
+        """
+
+        self.__size = size

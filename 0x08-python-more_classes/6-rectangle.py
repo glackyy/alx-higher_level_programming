5360dff0eb12f9646e5672821923987e51c74b4c
glackycode <glackycode@gmail.com>
2023-07-03 12:05:18 +0000

    6 rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,10 +7,10 @@ class Rectangle:
     """Represents a rectangle
 
     Attributes:
-        num_of_instances: (int)
+        number_of_instances: (int)
     """
 
-    num_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializing a new instance
@@ -19,7 +19,7 @@ class Rectangle:
             width: (int)
             height: (int)
         """
-        type(self).num_of_instances += 1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -88,5 +88,5 @@ class Rectangle:
 
     def __del__(self):
         """Print a message for every Del of a rectangle"""
-        type(self).num_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")

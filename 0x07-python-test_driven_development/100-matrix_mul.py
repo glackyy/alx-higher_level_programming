bdaa5cfea2eb3f8de8af96e68de10f7db41bc3f7
glackycode <glackycode@gmail.com>
2023-07-04 11:34:43 +0000

    fixes pycodestyle

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -14,15 +14,18 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list or m_b must be a list")
     if not all(isinstance(row, list) for row in m_a) or \
        not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+        raise TypeError("m_a must be a list of lists or" 
+			"m_b must be a list of lists")
     if len(m_a) == 0 or len(m_b) == 0:
         raise ValueError("m_a can't be empty or m_b can't be empty")
     if not all(isinstance(n, (int, float)) for row in m_a for n in row) or \
        not all(isinstance(n, (int, float)) for row in m_b for n in row):
-        raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
+        raise TypeError("m_a should contain only integers or floats or" 
+			"m_b should contain only integers or floats")
     if len(set(len(row) for row in m_a)) > 1 or \
        len(set(len(row) for row in m_b)) > 1:
-        raise TypeError("each row of m_a must be of the same size or each row of m_b must be of the same size")
+        raise TypeError("each row of m_a must be of the same size or" 
+			"each row of m_b must be of the same size")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
     res = []
@@ -33,4 +36,3 @@ def matrix_mul(m_a, m_b):
             row.append(e)
         res.append(row)
     return res
-

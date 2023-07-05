e198f5b86980c937950a6e5200011d72cf6d6058
glackycode <glackycode@gmail.com>
2023-07-05 08:40:33 +0000

    hot fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,13 +5,18 @@
 
 def add_integer(a, b=98):
     """Adds two integers.
- 
+
     Args:
         a: (int)
         b: (int)
 
     Returns: int
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-    return int(a) + int(b)
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+    return a + b

3bd50fb4977c561df5bba41c8524b83bd750452c
glackyy <othmanesticky@gmail.com>
2023-07-10 10:43:42 +0000

    is same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,6 +9,7 @@ def is_name_class(obj, a_class):
     Args:
         obj: The object to check
         a_class: the class to compare
+    Returns:
+           True
     """
     return type(obj) is a_class
-

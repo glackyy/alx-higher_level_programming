fa659ef4c70dd7ec798a3b11a8bf1907e93a76a0
glackycode <glackycode@gmail.com>
2023-07-10 10:03:17 +0000

    mylist

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defining a class MyList"""
+
+
+class Mylist(list):
+    """Custom list class that inherits from list"""
+    def print_sorted(self):
+        """Prints the list in sorted"""
+        sorted_l = sorted(self)
+        print(sorted_l)

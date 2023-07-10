b62c8b844d5ea1ec270571c881d09d59f28d48eb
glackycode <glackycode@gmail.com>
2023-07-10 10:31:31 +0000

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defining a function is_name_class"""
+
+
+def is_name_class(obj, a_class):
+    """Checks if the object is exactly an instance of the class
+
+    Args:
+        obj: The object to check
+        a_class: the class to compare
+    """
+    return type(obj) is a_class

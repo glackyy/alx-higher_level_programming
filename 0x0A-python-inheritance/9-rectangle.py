741501118b8f113601922d326b70fb51c4ff24f3
glackycode <glackycode@gmail.com>
2023-07-10 11:57:05 +0000

    8 rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defining a class Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class representing a Rectangle.Inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle
+
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+        Raises:
+            TypeError: If width or height is not an integer
+            ValueError: If width or height is not a positive integer
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+    def area(self):
+        """Compute the area of the rectangle
+
+        Returns:
+            int: The area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return a string representation of the Rectangle
+
+        Returns:
+            str: The string representation of the Rectangle
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

6f1ac9c8f435fa723b919a773586609c087f9728
glackycode <glackycode@gmail.com>
2023-07-11 09:36:54 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Defining write_file function"""
+
+
+def write_file(filename="", text=""):
+    """This function writes a string to a text file (UTF-8)
+
+    Args:
+        filename (str): The name of the file
+        text (str): The string to be written to the file
+    Returns: num_ch
+    """
+    with open(filename, mode='w', encoding='utf-8')
+        num_ch = file.write(text)
+    return num_ch

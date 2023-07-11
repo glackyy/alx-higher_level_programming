5b5f0ac211f28b4e983a8478368d714bfcb2c2ff
glackyy <othmanesticky@gmail.com>
2023-07-11 10:58:50 +0000

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Defining class_to_json function"""
+
+
+def class_to_json(obj):
+    """Returns the dict description of the obj for JSON serialization
+
+    Args:
+        obj: An instance of a class
+    """
+    json_dic = {}
+    for attrib in obj.__dict__:
+        val = getattr(obj, attrib)
+        if isinstance(val, (list, dict, str, int, bool)):
+            json_dic[attrib] = val
+    return json_dic

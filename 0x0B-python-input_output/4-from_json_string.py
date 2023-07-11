245c7121aceae0bfc9f49a0b4039ea357a69cb47
glackycode <glackycode@gmail.com>
2023-07-11 09:56:27 +0000

    from json string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Defining from_json_string function"""
+import json
+
+
+def from_json_string(my_str):
+    """This function returns an obj represented by a JSON string
+
+    Args:
+        my_str (str): The JSON string representing the obj
+    """
+    return json.loads(my_str)

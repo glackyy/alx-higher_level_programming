bcfbba84119a438a9a38f6f679ad66157f85b8df
glackyy <othmanesticky@gmail.com>
2023-07-11 11:24:40 +0000

    11 student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,23 +3,25 @@
 
 
 class Student:
-    """This class defines a student
+    """represents a student"""
 
-    Attributes:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
-    """
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns the dict representation of the student instance
-        
+        """Get a dictionary representation of the Student.
+
         Args:
-            attrs (list): The attributes names to be retrieved
+            attrs (list): (Optional) The attributes to represent.
         """
         if attrs is None:
             attrs = self.__dict__.keys()

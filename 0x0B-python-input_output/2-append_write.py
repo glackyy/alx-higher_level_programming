ad20c3c91f0d93df811ed202eec477e11ca9b61d
glackycode <glackycode@gmail.com>
2023-07-11 09:47:25 +0000

    append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Defining append_write function"""
+
+
+def append_write(filename="", text=""):
+    """This function appends a string at the end of a text file (UTF-8)
+
+    Args:
+        filename (str): The name of the file
+        text (str): The string to be appended to the file
+    Returns:
+        The number of characters added to the file
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        num_ch_added = file.write(text)
+    return num_ch_added

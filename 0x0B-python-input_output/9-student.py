4af2bc5a8f13649715791e3f992c951a98b32772
glackycode <glackycode@gmail.com>
2023-07-11 11:05:34 +0000

    student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Defining Student class"""
+
+
+class Student:
+    """This class defines a student
+
+    Attributes:
+        first_name (str): first name of the student
+        last_name (str): last name of the student
+        age (int): the age of the student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns te dict representation of the student instance"""
+        json_dic = {
+             'first_name': self.first_name,
+             'last_name': self.last_name,
+             'age': self.age
+          }
+        return json_dic

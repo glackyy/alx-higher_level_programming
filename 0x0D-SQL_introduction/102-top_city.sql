88147c64db8e8a8ec71a8a63c4e0e9d1b0a4268e
glackyy <othmanesticky@gmail.com>
2023-08-15 15:27:25 +0100

    task 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Displaying the 3 cities with the ħighest avg temperatures btw July and August
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;

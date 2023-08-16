05ce936d0783f742f769f7ec0b8d28ae6eca7443
glackyy <othmanesticky@gmail.com>
2023-08-16 14:52:20 +0100

    fixing cities by states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Listing all cities contained in the db hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.id = cities.states_id ORDER BY cities.id ASC;
+SELECT ct.id, ct.name, st.name FROM cities c JOIN states st  ON st.id = ct.states_id ORDER BY ct.id ASC;

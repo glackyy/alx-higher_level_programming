fc4d14d096fc5656277d33ce950f28e599428659
glackyy <othmanesticky@gmail.com>
2023-08-16 12:38:58 +0100

    fixing genre id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Listing all shows contained in the db hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genre.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

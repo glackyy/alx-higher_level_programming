ff633ea2d0f774fc02de97f7d7274990d82bb3a9
glackyy <othmanesticky@gmail.com>
2023-08-16 14:05:46 +0100

    shows genres linked to tvshows

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- Listing all shows, all genres linked to that show from hbtn_0d_tvshows
+SELECT title, name FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY title ASC, name ASC;

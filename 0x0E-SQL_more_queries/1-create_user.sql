183d403d3da83c2dcf8f7f8d2176f95f67285e4b
glackyy <othmanesticky@gmail.com>
2023-08-16 11:55:08 +0100

    update create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- creating user_0d_1
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- granting all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

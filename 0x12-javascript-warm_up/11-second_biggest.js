70670376ae0f971eb719060c4222c03b07de622f
glackyy <othmanesticky@gmail.com>
2023-09-11 11:58:40 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,7 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const ar = process.argv.slice(2).map(Number);
+  const sec = ar.sort(function (a, b) { return b - a; })[1];
+  console.log(sec);

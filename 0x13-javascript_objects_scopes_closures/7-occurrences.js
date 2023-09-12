006b60318b3af79dd1233bf74b818cd8d52b587a
glackyy <othmanesticky@gmail.com>
2023-09-12 11:48:16 +0100

    Occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nbOcurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let nOccurences = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {

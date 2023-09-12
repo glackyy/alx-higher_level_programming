d90a69b108c48c00680a694e83185f8c53e1c0c5
glackyy <othmanesticky@gmail.com>
2023-09-12 11:54:02 +0100

    esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,13 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let leng = list.length - 1;
+  let i = 0;
+  while ((leng - i) > 0) {
+    const es = list[leng];
+    list[leng] = list[i];
+    list[i] = es;
+    i++;
+    leng--;
+  }
+  return list;
+};

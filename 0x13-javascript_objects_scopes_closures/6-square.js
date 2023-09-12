9b40760088370cc1f078b84c8dfbda44d0054f6f
glackyy <othmanesticky@gmail.com>
2023-09-12 11:42:22 +0100

    Square charPrint()

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,19 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+const SquareX = require('./5-square');
+
+class Square extends SquareX {
+  charPrint (ch) {
+    if (ch === undefined) {
+      ch = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += ch;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;

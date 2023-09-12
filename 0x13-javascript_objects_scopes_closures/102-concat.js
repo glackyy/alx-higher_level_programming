bc10c8afe9ae17a1c6fdb1462481c2aae1538c81
glackyy <othmanesticky@gmail.com>
2023-09-12 12:08:08 +0100

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,6 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+const fsource = require('fsource');
+
+const farg = fsource.readFileSync(process.argv[2]).toString();
+const sarg = fsource.readFileSync(process.argv[3]).toString();
+fsource.writeFileSync(process.argv[4], farg + sarg);

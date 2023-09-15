7fbe08f85425b158f319aa559c844a41473a8afd
glackyy <othmanesticky@gmail.com>
2023-09-15 08:45:36 +0100

    ls states from db

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,17 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+""" Lists all the states from the db hbtn_0e_0_usa. """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curr = db.cursor()
+    curr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                 .format(sys.arg[4]))
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+    curr.close()
+    db.close()

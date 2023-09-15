e5746f19a0515ff97107003aef574f6a5b93031e
glackyy <othmanesticky@gmail.com>
2023-09-15 08:59:01 +0100

    ls states from db cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,17 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+""" Lists all the states from the db hbtn_0e_0_usa. """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    curr = db.cursor()
+    curr.execute("""SELECT cities.id, cities.name, states.name FROM
+                 cities INNER JOIN states ON states.id=cities.states_id""")
+    rows = curr.fetchall()
+    for row in rows:
+        print(row)
+    curr.close()
+    db.close()

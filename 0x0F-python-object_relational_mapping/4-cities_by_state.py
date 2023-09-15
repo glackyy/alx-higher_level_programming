1f0b6b32130d7299f12e641d2d898c0cc97f41f7
glackyy <othmanesticky@gmail.com>
2023-09-15 09:00:06 +0100

    ls states from db cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curr = db.cursor()
     curr.execute("""SELECT cities.id, cities.name, states.name FROM
-                 cities INNER JOIN states ON states.id=cities.states_id""")
+                 cities INNER JOIN states ON states.id=cities.state_id""")
     rows = curr.fetchall()
     for row in rows:
         print(row)

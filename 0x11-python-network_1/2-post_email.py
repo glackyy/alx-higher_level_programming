9265d24bd7bb3dda42e720ae721f452dd176ca4f
glackyy <othmanesticky@gmail.com>
2023-09-29 14:55:31 +0100

    POST email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,17 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Script that sends a POST request to the
+passed URL, takes the email as a parameter
+displays the body of the response
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    val = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(val).encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))

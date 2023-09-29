9653a8140b77a2db70135e2b652e46955f10b5c0
glackyy <othmanesticky@gmail.com>
2023-09-29 15:42:48 +0100

    json_api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,21 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Script that takes in a letter
+sends POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    pl = {"q": letter}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=pl)
+    try:
+        res = req.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")

86f0f5a28e37a3aa1f0475f7efedf65366bf2ff6
glackyy <othmanesticky@gmail.com>
2023-09-29 15:19:10 +0100

    POST email 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,14 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Script that takes in a URL,
+sends a request to the URL and displays the value
+of the X-Request-Id var found in the header of the response
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        print(dict(res.headers).get("X-Request-Id"))

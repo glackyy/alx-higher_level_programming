e5d578ed3cd88841107bf58a209ddc4d576e8330
glackyy <othmanesticky@gmail.com>
2023-09-29 15:10:57 +0100

    hbtn header 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Displaying the X-Request-Id header var of a request to a given URL
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.arg[1]
+    res = requests.get(url)
+    print(res.headers.get("X-Request-Id"))

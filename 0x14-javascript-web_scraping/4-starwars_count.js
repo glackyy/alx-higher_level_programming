5b3297b701d479b1aa59a1bc5cf5e6e2ff654be7
glackyy <othmanesticky@gmail.com>
2023-10-24 14:12:33 +0100

    starwas count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,8 @@
 const req = require('request');
 req(process.argv[2], function (error, response, body) {
   if (!error) {
-    const res = JSON.parse(body).res;
-    console.log(res.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;

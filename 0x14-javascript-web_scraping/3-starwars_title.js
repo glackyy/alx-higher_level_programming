a793b84ff09b643a1ae8b9e7137205c745c96d34
glackyy <othmanesticky@gmail.com>
2023-10-24 14:03:04 +0100

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
-#\!/usr/bin/node
+#!/usr/bin/node
+const req = require('request');
+const epnum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+req(API_URL + epnum, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const respJSON = JSON.parse(body);
+    console.log(respJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
